998f7794dc972a867d8acf336670587b706d6bb4
TufanGit <tufan1996karmakar@gmail.com>
2025-11-23 18:55:00 +0530

    code

---
+++ src/main/java/com/cicd/tufan/TufanApplication.java
@@ -0,0 +1,28 @@
+package com.cicd.tufan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class TufanApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome TUFAN";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(TufanApplication.class, args);
+	}
+
+//	echo "# github-actions-cicd" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin git@github.com:TufanGit/github-actions-cicd.git
+//	git push -u origin main
+
+}
---
+++ src/test/java/com/cicd/tufan/TufanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicd.tufan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TufanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
